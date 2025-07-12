58925b3c8506179603bb5c1fbfb1b35d1986da3c
Evzen Ponomarov <ponamarevevgeniy11@gmail.com>
2025-07-12 15:32:40 +0000

    The `npm install` command failed in my project. Analyze the following er

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+import js from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      'node_modules/**',
+      'slices/**',
+      '.next/**',
+      'build/**',
+      'dist/**',
+      'public/**',
+      'coverage/**',
+      '**/*.d.ts',
+      '.eslintrc.js',
+      'next.config.ts',
+      'tailwind.config.js',
+      'postcss.config.js',
+    ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      '@next/next': nextPlugin,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      next: {
+        rootDir: './',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended[0].rules,
+      ...tseslint.configs.recommended[1].rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }
+);
---
+++ .prettierrc.cjs
@@ -0,0 +1,28 @@
+// .prettierrc.js
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    printWidth: 100,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
+    importOrder: [
+        '^react$',
+        '^next$',
+        '^next/(.*)$',
+        '^@config/(.*)$',
+        '^@lib/(.*)$',
+        '^@store/(.*)$',
+        '^@hooks/(.*)$',
+        '^@types/(.*)$',
+        '^@components/(.*)$',
+        '^@styles/(.*)$',
+        '^@/(.*)$',
+        '^[./]',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
