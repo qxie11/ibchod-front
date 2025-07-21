fcbb24b7a427c6467dfb2ba37e1126cc507c01c2
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2025-07-21 19:39:11 +0200

    update: Deleted prismic.

--- src/shared/lib/utils/prismic-client.ts
+++
@@ -1,7 +0,0 @@
-import * as prismic from '@prismicio/client';
-
-export const repositoryName = 'iphone-market';
-
-export const createClient = (settings?: prismic.ClientConfig) => {
-  return prismic.createClient(repositoryName, settings || {});
-};
