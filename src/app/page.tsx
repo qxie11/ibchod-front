5893c1428a47f995cb9e2bba975721cb64347706
Dev team <devteam@mail.com>
2025-10-10 21:24:39 +0200

    test stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import HomePage from '@/pages-components';
 import { GetProductsResponse } from '@/shared/lib/slices/productApi';
 
 export const metadata: Metadata = {
-  title: 'Repasované iPhony se zárukou | IObchod',
+  title: 'test stage | IObchod',
   description:
     'Objevte široký výběr repasovaných iPhonů v IObchod. Všechny modely se zárukou 3 měsíců, rychlým doručením a za skvělé ceny. Udržitelná volba pro vás i planetu.',
   keywords: [
